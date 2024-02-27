6e178a8f276dc4eef3c42e49146286451f9ccfe3
dabote <45696952+dabote@users.noreply.github.com>
2024-02-27 19:17:33 +0100

    BinaereSucheIterativ hinzugefügt!

--- SucheVergleichBinaerLinear/BinaereSucheWrapper.java
+++ SucheVergleichBinaerLinear/BinaereSucheWrapper.java
@@ -39,6 +39,20 @@ public class BinaereSucheWrapper
 
     }
             
+    public void testeBinaereSucheIterativ( int anzahlVersuche )
+    {
+        long timeStart;
+        long timeEnd;
+        
+        timeStart = System.currentTimeMillis();
+        for( int i=0; i<anzahlVersuche; i++){
+            binaereSucheIterativ( randomGenerator.nextInt(10000) );    
+        }
+        timeEnd = System.currentTimeMillis();
+        
+        System.out.println("Laufzeit binäre Suche iterativ: " + anzZahlen + " Elemente, " + anzahlVersuche + " Versuche, " + (timeEnd - timeStart) + " Millisek.");
+
+    }
             
 
     public void listeFuellen( )
@@ -63,29 +77,65 @@ public class BinaereSucheWrapper
     
     public boolean binaereSucheRekursiv(int i, int lowerBound, int upperBound)
     {
+
         // idx berechnen
-        //int idx = 
+        int idx = (int)(lowerBound + upperBound ) / 2;
         
         // Abbruch der Suche
-        // if(  ){
-            // return false;
-        // }
+        if( lowerBound > upperBound || idx == anzZahlen ){
+            return false;
+        }
         
-        // // Pruefen ob gefunden
-        // if(  ){
-            // return true;
-        // }
-        // // gesuchte Zahl ist kleiner als idx-Zahl
-        // else if(  ){
-            // return binaereSucheRekursiv( );
-        // }
-        // // gesuchte Zahl ist größer als idx-Zahl
-        // else{
-            // return binaereSucheRekursiv( );
-        // }
-       
-        return false;
+        // Pruefen ob gefunden
+        if( arrayZahlen[idx] == i ){
+            //System.out.println( "Nicht gefunden: Anzahl Schritte: " + anzahlSchritte);
+            return true;
+        }
+        // gesuchte Zahl ist kleiner als idx-Zahl
+        else if( i < arrayZahlen[idx] ){
+            return binaereSucheRekursiv(i, lowerBound, idx-1 );
+        }
+        // gesuchte Zahl ist größer als idx-Zahl
+        else{
+            return binaereSucheRekursiv(i, idx+1, upperBound);
+        }
         
+              
+        
+    
+    }
+    
+    
+    public boolean binaereSucheIterativ(int i)
+    {
+        int lowerBound = 0;
+        int upperBound = anzZahlen;
+        int idx = -2;
+        int anzahlSchritte = 0;
+                
+        while( true ){
+            anzahlSchritte++;
+            // idx berechnen
+            idx = (int)( (lowerBound + upperBound ) / 2 );
+            
+            // Suche abbrechen
+            if( (lowerBound > upperBound) || (idx == anzZahlen) ){
+                //System.out.println( "Nicht gefunden: Anzahl Schritte: " + anzahlSchritte);
+                return false;
+            // Wenn gesuchtes Element < arrayZahlen[idx] -> links suchen            
+            }else if( i < arrayZahlen[idx] ){
+                // lowerBound bleibt unverändert
+                upperBound = idx - 1;
+            // Wenn gesuchtes Element < arrayZahlen[idx] -> links suchen
+            }else if( i > arrayZahlen[idx] ){
+                lowerBound = idx + 1;
+                // upperBound bleibt unverändert
+            }else{ // i == arrayZahlen
+                 //System.out.println( "Gefunden!!: Anzahl Schritte: " + anzahlSchritte);
+                 return true;
+            }
+            
+        }
     }
     
     
